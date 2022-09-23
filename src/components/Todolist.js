99b4aadf5a78f15bf56f006c95b68889b4444353
Lotta Laine <lotta-laine@hotmail.com>
2022-09-23 14:54:01 +0300

    first commit

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,23 +1,35 @@
 import React, { useState } from 'react';
 
 function Todolist() {
-	const [description, setDescription] = useState('');
+	const [input, setInput] = useState({
+		description: '',
+		date: '',
+	});
 	const [todos, setTodos] = useState([]);
 
 	function addTodo() {
-		setTodos([description, ...todos]);
+		setTodos([input, ...todos]);
 	}
 
 	return (
 		<div className='App'>
-			<input placeholder="Something to do" value={description} onChange={event => setDescription(event.target.value)} />
+			<h2>Add todo:</h2>
+			Date:
+			<input name="date" placeholder="dd.mm.yyyy" value={input.date} onChange={event => setInput({ ...input, [event.target.name]: event.target.value })} />
+			Description:
+			<input name="description" placeholder="Something to do" value={input.description} onChange={event => setInput({ ...input, [event.target.name]: event.target.value })} />
 			<button onClick={addTodo}>Add</button>
 			<table>
 				<tbody>
+					<tr>
+						<th>Date</th>
+						<th>Description</th>
+					</tr>
 					{
-						todos.map(todo =>
-							<tr>
-								<td>{todo}</td>
+						todos.map((todo) =>
+							<tr key={todo.description}>
+								<td>{todo.date}</td>
+								<td>{todo.description}</td>
 							</tr>
 						)
 					}
