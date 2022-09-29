b7dea162dceb3d468a7f0cd77a53c74a16829e92
Lotta Laine <lotta-laine@hotmail.com>
2022-09-29 09:25:54 +0300

    Separate todotable into a new component

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import TodoTable from './TodoTable';
 
 function Todolist() {
 	const [input, setInput] = useState({
@@ -23,24 +24,7 @@ function Todolist() {
 			Description:
 			<input name="description" placeholder="Something to do" value={input.description} onChange={event => setInput({ ...input, [event.target.name]: event.target.value })} />
 			<button onClick={addTodo}>Add</button>
-			<table>
-				<tbody>
-					<tr>
-						<th>Date</th>
-						<th>Description</th>
-						<th></th>
-					</tr>
-					{
-						todos.map((todo, index) =>
-							<tr key={index}>
-								<td>{todo.date}</td>
-								<td>{todo.description}</td>
-								<td><button onClick={() => deleteTodo(index)}>Delete</button></td>
-							</tr>
-						)
-					}
-				</tbody>
-			</table>
+			<TodoTable todos={todos} deleteTodo={deleteTodo} />
 		</div>
 	);
 }
