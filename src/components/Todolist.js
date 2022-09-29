2fad01f131a889a24a49c4b3b3f91f6d881eac61
Lotta Laine <lotta-laine@hotmail.com>
2022-09-29 09:05:54 +0300

    Add delete button

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -11,6 +11,10 @@ function Todolist() {
 		setTodos([input, ...todos]);
 	}
 
+	function deleteTodo(row) {
+		setTodos(todos.filter((item, i) => row !== i));
+	}
+
 	return (
 		<div className='App'>
 			<h2>Add todo:</h2>
@@ -24,12 +28,14 @@ function Todolist() {
 					<tr>
 						<th>Date</th>
 						<th>Description</th>
+						<th></th>
 					</tr>
 					{
-						todos.map((todo) =>
-							<tr key={todo.description}>
+						todos.map((todo, index) =>
+							<tr key={index}>
 								<td>{todo.date}</td>
 								<td>{todo.description}</td>
+								<td><button onClick={() => deleteTodo(index)}>Delete</button></td>
 							</tr>
 						)
 					}
