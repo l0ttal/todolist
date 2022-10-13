f8f30ae5c642286c7f533a0076b46735a60dd4fc
Lotta Laine <lotta-laine@hotmail.com>
2022-10-13 15:36:31 +0300

    create components and nav menu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import './App.css';
 import Todolist from './components/Todolist';
+import Home from './components/Home';
+import About from './components/About';
+import NotFound from './components/NotFound';
+
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6">
-            My todos
+          <Typography variant="h3">
+            Welcome
           </Typography>
-        </Toolbar>
-      </AppBar>
-      <Todolist />
-    </div>
+        </Toolbar >
+      </AppBar >
+      <BrowserRouter>
+        <Link to="/">Home</Link>{' '}
+        <Link to="/about">About</Link>{' '}
+        <Link to="/todolist">Todo list</Link>{' '}
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/todolist" element={<Todolist />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div >
   );
 }
 
